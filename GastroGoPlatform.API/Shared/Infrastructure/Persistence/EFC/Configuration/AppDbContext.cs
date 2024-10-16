9c013cfb8d096c3d1caa68d68ac1ca7a6d6032e8
CamilaLoli <u202110385@upc.edu.pe>
2024-10-16 02:25:14 -0500

    feat(orm): automatic table name pluralization.

---
+++ GastroGoPlatform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace GastroGoPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddCreatedUpdatedInterceptor();
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        // TODO
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        // }
+    }
+}
