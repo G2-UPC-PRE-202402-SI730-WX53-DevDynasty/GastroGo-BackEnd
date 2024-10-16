4dd4381cc4c954dfde37daaa672453ce433d5146
CamilaLoli <u202110385@upc.edu.pe>
2024-10-16 01:46:00 -0500

    chore: clean project.

--- GastroGoPlatform.API/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GastroGoPlatform.API.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
