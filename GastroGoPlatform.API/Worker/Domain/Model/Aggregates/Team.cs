8a7140b08b8f82da8205cb3aa51178955f709c3c
CamilaLoli <u202110385@upc.edu.pe>
2024-10-21 01:51:32 -0500

    feat(team-activities): added aggregate for Team, entities for TeamLeader and TeamMember, and ValueObject for the available roles.

---
+++ GastroGoPlatform.API/Worker/Domain/Model/Aggregates/Team.cs
@@ -0,0 +1,13 @@
+﻿namespace GastroGoPlatform.API.Worker.Domain.Model.Aggregates
+{
+    /// Team Aggregate
+    /// <summary>
+    ///     This class represents the Team Aggregate. Used to store each team and its respective leader and members. 
+    /// </summary>
+    public class Team
+    { 
+        public int Id { get; set; }
+        public required string TeamName { get; set; }
+        
+    }
+}
---
+++ GastroGoPlatform.API/Worker/Domain/Model/Entities/TeamLeader.cs
@@ -0,0 +1,22 @@
+﻿using GastroGoPlatform.API.Worker.Domain.Model.Valueobjects;
+
+namespace GastroGoPlatform.API.Worker.Domain.Model.Entities
+{
+    /// TeamLeader Entity
+    /// <summary>
+    ///     This is the TeamLeader entity. It represents the leader of a team.
+    /// </summary>
+    public class TeamLeader
+    {
+        public Guid LeaderId { get; set; }
+        public int UserId { get; set; }
+        public TeamRole Role { get; set; }
+
+        public TeamLeader(int userId)
+        {
+            LeaderId = Guid.NewGuid();
+            UserId = userId;
+            Role = TeamRole.Leader;
+        }
+    }
+}
