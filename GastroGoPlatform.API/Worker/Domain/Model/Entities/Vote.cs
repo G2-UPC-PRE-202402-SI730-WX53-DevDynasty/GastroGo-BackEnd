fff92a2d59debd4e1a215001cfe2e2924826b770
CamilaLoli <u202110385@upc.edu.pe>
2024-10-21 23:35:45 -0500

    feat(team-activities): added vote entity.

---
+++ GastroGoPlatform.API/Worker/Domain/Model/Entities/Vote.cs
@@ -0,0 +1,11 @@
+﻿namespace GastroGoPlatform.API.Worker.Domain.Model.Entities
+{
+    public class Vote
+    {
+        public int Id { get; set; }
+        /// <summary>
+        ///     Dictionary of votes for each restaurant
+        /// </summary>
+        public required Dictionary<string, int> Votes { get; set; }
+    }
+}
