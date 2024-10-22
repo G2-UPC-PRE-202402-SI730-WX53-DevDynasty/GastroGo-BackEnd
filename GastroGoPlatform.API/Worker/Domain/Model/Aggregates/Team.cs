f913a1dc4e249c2af82b09f0c1bf7c7b7a061aef
CamilaLoli <u202110385@upc.edu.pe>
2024-10-21 19:52:50 -0500

    feat(team-activities): added constructor and methods to add or remove a member.

--- GastroGoPlatform.API/Worker/Domain/Model/Aggregates/Team.cs
+++ GastroGoPlatform.API/Worker/Domain/Model/Aggregates/Team.cs
@@ -1,4 +1,5 @@
-﻿namespace GastroGoPlatform.API.Worker.Domain.Model.Aggregates
+﻿using GastroGoPlatform.API.Worker.Domain.Model.Entities;
+namespace GastroGoPlatform.API.Worker.Domain.Model.Aggregates
 {
     /// Team Aggregate
     /// <summary>
@@ -6,8 +7,24 @@
     /// </summary>
     public class Team
     { 
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public required string TeamName { get; set; }
-        
+        public required TeamLeader Leader { get; set; }
+        public required List<TeamMember> Members { get; set; }
+        public Team(string teamName, TeamLeader leader)
+        {
+            Id = Guid.NewGuid();
+            TeamName = teamName;
+            Leader = leader;
+            Members = new List<TeamMember>();
+        }
+        public void AddMember(TeamMember teamMember)
+        {
+            Members.Add(teamMember);
+        }
+        public void RemoveMember(TeamMember teamMember)
+        {
+            Members.Remove(teamMember);
+        }
     }
 }
