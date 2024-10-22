e818a251854d8ce0ed927fe4f12cb04ca93b9955
CamilaLoli <u202110385@upc.edu.pe>
2024-10-21 19:55:44 -0500

    feat(team-activities): added command to create a Team.

--- GastroGoPlatform.API/Worker/Domain/Model/Aggregates/Team.cs
+++ GastroGoPlatform.API/Worker/Domain/Model/Aggregates/Team.cs
@@ -1,4 +1,5 @@
-﻿using GastroGoPlatform.API.Worker.Domain.Model.Entities;
+﻿using GastroGoPlatform.API.Worker.Domain.Model.Commands;
+using GastroGoPlatform.API.Worker.Domain.Model.Entities;
 namespace GastroGoPlatform.API.Worker.Domain.Model.Aggregates
 {
     /// Team Aggregate
@@ -7,17 +8,24 @@ namespace GastroGoPlatform.API.Worker.Domain.Model.Aggregates
     /// </summary>
     public class Team
     { 
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public required string TeamName { get; set; }
         public required TeamLeader Leader { get; set; }
         public required List<TeamMember> Members { get; set; }
         public Team(string teamName, TeamLeader leader)
         {
-            Id = Guid.NewGuid();
             TeamName = teamName;
             Leader = leader;
             Members = new List<TeamMember>();
         }
+
+        public Team(CreateTeamCommand command)
+        {
+            this.Id = command.TeamId;
+            this.TeamName = command.TeamName;
+            this.Leader = new TeamLeader(command.LeaderId);
+        }
+        
         public void AddMember(TeamMember teamMember)
         {
             Members.Add(teamMember);
