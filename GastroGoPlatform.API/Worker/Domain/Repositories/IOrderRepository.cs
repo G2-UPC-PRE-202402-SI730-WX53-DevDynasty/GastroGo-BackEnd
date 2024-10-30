8749ee7cd92b86b26cedf86b3037b29c8c260ce3
FABIOLA <ximemegan1302@gmail.com>
2024-10-30 15:01:37 -0500

    feat(order): added repository for Order

---
+++ GastroGoPlatform.API/Worker/Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using GastroGoPlatform.API.Shared.Domain.Repositories;
+
+namespace GastroGoPlatform.API.Worker.Domain.Repositories
+{
+    public interface IOrderRepository: IBaseRepository<Order>
+    {
+        Task<IEnumerable<Order>> FindByCustomerIdAsync(int customerId);
+        Task<Order?> FindByOrderIdAsync(int orderId);
+    }
+}
+
+
+
