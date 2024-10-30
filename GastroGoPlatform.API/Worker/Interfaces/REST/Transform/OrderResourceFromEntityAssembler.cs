73be5233d08a6c25870c559c3b3d732d7bb3c3a1
FABIOLA <ximemegan1302@gmail.com>
2024-10-30 15:07:43 -0500

    feat(order): added pattern assembler for orden

---
+++ GastroGoPlatform.API/Worker/Interfaces/REST/Transform/OrderResourceFromEntityAssembler.cs
@@ -0,0 +1,10 @@
+﻿using GastroGoPlatform.API.Worker.Interfaces.REST.Resources;
+
+namespace GastroGoPlatform.API.Worker.Interfaces.REST.Transform
+{
+    public static class OrderResourceFromEntityAssembler
+    {
+        public static OrderResource ToResourceFromEntity(Order entity) =>
+            new OrderResource(entity.OrderId);
+    }
+}
