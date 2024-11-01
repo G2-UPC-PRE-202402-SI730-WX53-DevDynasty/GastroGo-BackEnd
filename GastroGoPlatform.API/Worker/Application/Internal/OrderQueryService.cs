32da9dfbdd11d28bd91a2db4683bcb3077eeafda
Marco <m.gongora.sanchez08@gmail.com>
2024-10-31 19:09:50 -0500

    feat(OrderQueryService): implement methods to handle order queries

--- GastroGoPlatform.API/Worker/Application/Internal/OrderQueryService.cs
+++ GastroGoPlatform.API/Worker/Application/Internal/OrderQueryService.cs
@@ -1,27 +1,36 @@
-﻿using GastroGoPlatform.API.Shared.Domain.Repositories;
+﻿using GastroGoPlatform.API.Booking.Domain.Repositories;
+using GastroGoPlatform.API.Shared.Domain.Repositories;
+using GastroGoPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 using GastroGoPlatform.API.Worker.Domain.Model.Commnads;
 using GastroGoPlatform.API.Worker.Domain.Model.Queries;
 using GastroGoPlatform.API.Worker.Domain.Repositories;
 using GastroGoPlatform.API.Worker.Domain.Services.Command;
 using GastroGoPlatform.API.Worker.Domain.Services.Query;
+using GastroGoPlatform.API.Worker.Infrastructure.Repositories;
 
 namespace GastroGoPlatform.API.Worker.Application.Internal
 {
-    public class OrderQueryService : IOrderQueryService
+    public class OrderQueryService(IOrderRepository orderRepository, IUnitOfWork unitOfWork) : IOrderQueryService
     {
-        public Task<IEnumerable<Order>> Handle(GetAllOrdersQuery query)
+        public async Task<IEnumerable<Order>> Handle(GetAllOrdersQuery query)
         {
-            throw new NotImplementedException();
+            var orders = await orderRepository.ListAsync();
+            await unitOfWork.CompleteAsync();
+            return orders;
         }
 
-        public Task<Order?> Handle(GetOrderByIdQuery query)
+        public async Task<Order?> Handle(GetOrderByIdQuery query)
         {
-            throw new NotImplementedException();
+            var order = await orderRepository.FindByOrderIdAsync(query.Id);
+            await unitOfWork.CompleteAsync();
+            return order;
         }
 
-        public Task<Order?> Handle(GetOrderByCustomerIdQuery query)
+        public async Task<IEnumerable<Order>> Handle(GetOrderByCustomerIdQuery query)
         {
-            throw new NotImplementedException();
+            var orders = await orderRepository.FindByCustomerIdAsync(query.CustomerId);
+            await unitOfWork.CompleteAsync();
+            return orders;
         }
     }
 
