148b06cbe10e6df89effaa8ef7c643c0e77a9809
Marco <m.gongora.sanchez08@gmail.com>
2024-10-31 19:07:23 -0500

    fix(IOrderQueryService ): update return types for GetAllOrders and GetOrderByCustomerId

--- GastroGoPlatform.API/Worker/Domain/Services/Query/IOrderQueryService.cs
+++ GastroGoPlatform.API/Worker/Domain/Services/Query/IOrderQueryService.cs
@@ -1,11 +1,12 @@
-﻿using GastroGoPlatform.API.Worker.Domain.Model.Queries;
+﻿using GastroGoPlatform.API.Worker.Domain.Model.Entities;
+using GastroGoPlatform.API.Worker.Domain.Model.Queries;
 
 namespace GastroGoPlatform.API.Worker.Domain.Services.Query
 {
     public interface IOrderQueryService
     {
-        Task<Order?> Handle(GetAllOrdersQuery query);
+        Task<IEnumerable<Order>> Handle(GetAllOrdersQuery query);
         Task<Order?> Handle(GetOrderByIdQuery query);
-        Task<Order?> Handle(GetOrderByCustomerIdQuery query);
+        Task<IEnumerable<Order>> Handle(GetOrderByCustomerIdQuery query);
     }
 }
