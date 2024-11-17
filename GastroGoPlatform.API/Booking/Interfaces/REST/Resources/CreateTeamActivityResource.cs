52985b47c677d7e1094184f72e8051089c3ca774
CamilaLoli <u202110385@upc.edu.pe>
2024-11-17 13:01:36 -0500

    feat(booking): created resources for team activity.

---
+++ GastroGoPlatform.API/Booking/Interfaces/REST/Resources/CreateTeamActivityResource.cs
@@ -0,0 +1,8 @@
+﻿namespace GastroGoPlatform.API.Booking.Interfaces.REST.Resources;
+
+/// CreateTeamActivityResource
+/// <summary>
+/// This is the resource for creating a team.
+/// </summary>
+
+public record CreateTeamActivityResource(string Name);
---
+++ GastroGoPlatform.API/Booking/Interfaces/REST/Resources/TeamActivityResource.cs
@@ -0,0 +1,5 @@
+﻿using GastroGoPlatform.API.Booking.Domain.Model.Entities;
+
+namespace GastroGoPlatform.API.Booking.Interfaces.REST.Resources;
+
+public record TeamActivityResource(int TeamId, string Name, IEnumerable<Role> Roles);
