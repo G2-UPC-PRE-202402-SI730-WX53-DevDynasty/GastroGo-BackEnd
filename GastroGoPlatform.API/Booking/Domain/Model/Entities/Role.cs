c126aa7afd1110c9fe4aace98552c51f59d5d205
CamilaLoli <u202110385@upc.edu.pe>
2024-11-17 09:33:24 -0500

    feat(booking): added role class to manage the role assigment.

---
+++ GastroGoPlatform.API/Booking/Domain/Model/Entities/Role.cs
@@ -0,0 +1,30 @@
+﻿
+using GastroGoPlatform.API.Booking.Domain.Model.ValueObjects;
+using Microsoft.AspNetCore.Identity;
+
+namespace GastroGoPlatform.API.Booking.Domain.Model.Entities
+{
+    /// Role Entity
+    /// <summary>
+    ///     This represents the Role Entity. Used to store the roles of team leaders or members.
+    ///     This defines
+    /// </summary>
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public int UserId { get; set; }
+        public ERoles RoleType { get; set; }
+        
+        public Role(int userId, ERoles roleType)
+        {
+            UserId = userId;
+            RoleType = roleType;
+        }
+        
+        public static Role AssignRole(int userId, ERoles roleType)
+        {
+            return new Role(userId, roleType);
+        }
+        
+    }
+}
