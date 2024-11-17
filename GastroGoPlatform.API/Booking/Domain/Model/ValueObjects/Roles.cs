33d0b17effc297d366b0f3395435e61cda356017
CamilaLoli <u202110385@upc.edu.pe>
2024-11-17 00:03:42 -0500

    feat(booking): added roles enum to define roles of member and leader.

---
+++ GastroGoPlatform.API/Booking/Domain/Model/ValueObjects/Roles.cs
@@ -0,0 +1,12 @@
+﻿namespace GastroGoPlatform.API.Booking.Domain.Model.ValueObjects
+{
+    /// Role Value Object
+    /// <summary>
+    ///    This class represents the Role Value Object. Used to store the roles of team leaders or members.
+    /// </summary>
+    public enum Roles
+    {
+        Leader,
+        Member
+    }
+}
